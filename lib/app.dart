ee9670489d733ed7c977f22e968b760489ee925e
baophongbui <phongtin765@gmail.com>
2025-07-09 12:21:16 +0700

    update_darkmode

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,10 @@
+import 'package:cross_website/components/cookie_consent_banner.dart';
+import 'package:cross_website/components/header.dart';
 import 'package:cross_website/constants/app_colors.dart';
 import 'package:cross_website/language/language_manager.dart';
 import 'package:cross_website/pages/about_new.dart';
+import 'package:cross_website/pages/loading_screen.dart';
+import 'package:cross_website/pages/not_found_page.dart';
 import 'package:jaspr/jaspr.dart';
 import 'package:jaspr_riverpod/jaspr_riverpod.dart';
 import 'package:jaspr_router/jaspr_router.dart';
@@ -24,6 +28,12 @@ class AppState extends State<App> {
   bool _isLoading = true;
   bool _hasError = false;
 
+  @override
+  void initState() {
+    super.initState();
+    _loadTranslations();
+  }
+
   Future<void> _loadTranslations() async {
     try {
       final success = await LanguageManager.loadTranslations();
@@ -44,31 +54,48 @@ class AppState extends State<App> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    _loadTranslations();
-  }
-
-  @override
   Iterable<Component> build(BuildContext context) sync* {
-    yield ProviderScope(
-      child: div(classes: 'main', [
-        Router(
-          routes: [
-            Route(
-              path: '/',
-              title: 'Home',
-              builder: (context, state) => const Home(),
-            ),
-            Route(
-              path: '/about',
-              title: 'About',
-              builder: (context, state) => const AboutNew(),
-            ),
-          ],
-        ),
-      ]),
-    );
+    if (_isLoading) {
+      yield LoadingScreen();
+    } else if (_hasError) {
+      yield div(classes: 'main', [text('Error loading translations')]);
+    } else {
+      yield ProviderScope(
+        child: div(classes: 'main', [
+          CookieConsentBanner(),
+          Router(
+            routes: [
+              Route(
+                path: '/',
+                title: 'Home',
+                builder: (context, state) => div(classes: 'main', [
+                  // Header(),
+                  const Home(),
+                ]),
+              ),
+              Route(
+                path: '/about',
+                title: 'About',
+                builder: (context, state) => div(classes: 'main', [
+                  // Header(),
+                  const AboutNew(),
+                ]),
+              ),
+              Route(
+                path: '/:path',
+                builder: (context, state) {
+                  final currentPath = state.path;
+                  if (currentPath != '/' && currentPath != '/about') {
+                    return const NotFoundPage();
+                  }
+                  return div([]);
+                },
+              ),
+            ],
+          ),
+        ]),
+      );
+    }
   }
 
   @css
@@ -86,25 +113,6 @@ class AppState extends State<App> {
             justifyContent: JustifyContent.center,
             alignItems: AlignItems.center,
             flex: Flex(grow: 1),
-          ),
-        ]),
-        css('.loading-container', [
-          css('&').styles(
-            display: Display.flex,
-            height: 100.vh,
-            width: 100.vw,
-            justifyContent: JustifyContent.center,
-            alignItems: AlignItems.center,
-            backgroundColor: AppColors.backgroundTheme,
-          ),
-        ]),
-        css('.loading-spinner', [
-          css('&').styles(
-            display: Display.flex,
-            flexDirection: FlexDirection.column,
-            alignItems: AlignItems.center,
-            justifyContent: JustifyContent.center,
-            textAlign: TextAlign.center,
           ),
         ]),
       ];
